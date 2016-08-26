d3f96199e830b42573fd7618fd5869aec245d3f9
Leo <HooHu711@Gmail.com>
2016-08-26 18:50:24 +1000

    added more cgharacters

--- game.js
+++ game.js
@@ -1,15 +1,22 @@
+var game = {
+	start: function() {
+		var readline = require('readline');
 
-var readline = require('readline');
+		var readlineThing = readline.createInterface({
+		    input: process.stdin,
+		    output:process.stdout
+		});
 
-var readlineThing = readline.createInterface({
-    input: process.stdin,
-    output:process.stdout
-});
+		readlineThing.question("what is your name ", function(answer){
+		console.log('Hi there ' + answer + ", welcome to the game!")
+		readlineThing.close();
+		})
 
-readlineThing.question("what is your name ", function(answer){
-console.log('Hi there ' + answer + ", welcome to the game!")
-readlineThing.close();
-})
+	},
+	restart: function(){
+		game.start();
+	}
+}
 
 //console.log("This Game works!");
 
@@ -31,6 +38,10 @@ var Player = {
 		console.log("you are on full health. Stop eating.");
 	} else {
 		health = healh + 2
+		//Name
+//Race
+//Class
+//Health
 	
 
 	}
@@ -54,12 +65,53 @@ var finalBoss = {
 		}
 	}
 }
+function chooseClass(answer) {
+  Player.class = answer;
+  Player.skill["1"] = function() {
+    console.log("Take this @#%!@#@!#!@!");
+    finalBoss.health = finalBoss.health - 10;
+  }
+  if (Player.class == "Mage") {
+    //Player.health = 10;
+    Player.skill["2"] = function() {
+      console.log("Eat fire");
+      finalBoss.health = finalBoss.health - 20;
+    }
+    Player.skill["3"] = function() {
+      console.log("I haz more health");
+      if (Player.health > 10) {
+        Player.health = 20;
+      } else {
+          Player.health = Player.health + 10;
+      }
+    }
+  } else if (Player.class == "Thief") {
+    //Player.health = 10;
+    Player.skill["2"] = function() {
+      console.log("Stabby stab stab");
+      finalBoss.health = finalBoss.health - 20;
+    }
+    Player.skill["3"] = function() {
+      console.log("Eat my arrow");
+      finalBoss.health = finalBoss.health - 15;
+    }
+  } else if (Player.class == "Warrior") {
+    //Player.health = 10;
+    Player.skill["2"] = function() {
+      console.log("Shanking intensifies");
+      finalBoss.health = finalBoss.health - 10;
+      console.log("Shanking intensifies");
+      finalBoss.health = finalBoss.health - 10;
+    }
+    Player.skill["3"] = function() {
+      console.log("This skill is a placeholder. It does nothing.");
+    }
+  }
+}
 
-console.log(finalBoss)
+
+game.start()
 
 
 
-//Name
-//Race
-//Class
-//Health
+
